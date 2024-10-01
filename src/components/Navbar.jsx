2ba2670643b5d538541ce6e2ee7e9dafa6aa2506
MohummadRafique0 <yomoplaz@gmail.com>
2024-10-01 09:18:28 +0500

    added iTaask files to repository

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-around bg-indigo-900 text-white py-4'>
+        <div className="logo">
+            <span className='font-bold text-4xl mx-0'>ITask</span>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
